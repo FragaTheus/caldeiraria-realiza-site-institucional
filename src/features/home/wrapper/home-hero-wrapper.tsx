9ee904fe04c01d9eec8b75057df7ca9c5d404e0c
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2026-03-23 17:47:54 -0300

    feat: Atualizando video hero home

--- src/features/home/wrapper/home-hero-wrapper.tsx
+++ src/features/home/wrapper/home-hero-wrapper.tsx
@@ -1,7 +1,4 @@
 import AppSectionWrapper from "@/shared/wrapper/section-wrapper";
-import heroImg from "@/shared/assets/hero-img.jpeg";
-import heroImg1 from "@/shared/assets/hero-img1.jpeg";
-import heroImg2 from "@/shared/assets/hero-img2.jpeg";
 import heroTextContent from "@/features/home/text-content/heroTextContent.json";
 import AppHeroCard from "../components/home-hero-card";
 
@@ -19,5 +16,3 @@ const HomeHeroWrapper = () => {
 };
 
 export default HomeHeroWrapper;
-
-const imageSrc = [heroImg, heroImg1, heroImg2];
