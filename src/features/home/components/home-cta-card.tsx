cbe3aaad6484eb2f5b4590b6146d360b8c5b39d5
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2026-03-24 19:09:14 -0300

    refac: Implementando nova headline e refatorando tamanho texto formulario

--- src/features/home/components/home-cta-card.tsx
+++ src/features/home/components/home-cta-card.tsx
@@ -9,7 +9,7 @@ const HomeCtaCard = ({ title }: HomeCtaCardProps) => {
   return (
     <article className="w-full text-white flex flex-col items-center justify-center  border border-white p-4 lg:p-8 rounded-sm text-center gap-8">
       <AnimateTitle>
-        <h1 className="whitespace-pre-line">{title}</h1>
+        <h2 className="whitespace-pre-line lg:h-1">{title}</h2>
       </AnimateTitle>
       <AppForm />
     </article>
