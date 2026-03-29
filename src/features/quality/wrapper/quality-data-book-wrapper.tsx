0481c5bc7e798889a5d4ae5796e60b450d1e4a52
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2026-03-29 14:23:25 -0300

    reafac: Corrigindo erro de build

--- src/features/quality/wrapper/quality-data-book-wrapper.tsx
+++ src/features/quality/wrapper/quality-data-book-wrapper.tsx
@@ -23,26 +23,32 @@ const QualityDataBookWrapper = () => {
 
 const cards = [
   {
+    index: 0,
     Icon: TbEye,
     description: textContent.databook_section.items.item_1,
   },
   {
+    index: 1,
     Icon: TbDroplet,
     description: textContent.databook_section.items.item_2,
   },
   {
+    index: 2,
     Icon: TbUserCheck,
     description: textContent.databook_section.items.item_3,
   },
   {
+    index: 3,
     Icon: TbClipboardList,
     description: textContent.databook_section.items.item_4,
   },
   {
+    index: 4,
     Icon: TbRulerMeasure,
     description: textContent.databook_section.items.item_5,
   },
   {
+    index: 5,
     Icon: TbAtom,
     description: textContent.databook_section.items.item_6,
   },
