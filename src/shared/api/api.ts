8aae7b0fb1c6fdfa3ea55eab7af3edefd41b2747
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2026-04-22 23:08:24 -0300

    Atualizando endereco da api

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create(
     {
-        baseURL: "http://localhost:8080/api/v1",
+        baseURL: "https://realiza-mail-service-latest.onrender.com/api/v1",
        
     }
 )
